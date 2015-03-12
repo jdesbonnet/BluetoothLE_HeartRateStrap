c2a2ac43a9ba8703e84b92ac963b77b5efd161a7
Joe Desbonnet <jdesbonnet@gmail.com>
2015-03-12 00:24:47 +0000

    tidy

--- scripts/btle_hr_stream_data.sh
+++ scripts/btle_hr_stream_data.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Script to connect to Bluetooth LE heart rate monitor and display
-# time (unix epoc time) and heart rate (bpm).
+# Script to connect to Bluetooth LE (low energy aka Bluetooth SMART) heart 
+# rate monitor and stream time (unix epoc time) and heart rate (bpm) to stdout.
+#
+# Requires a reasonably up-to-date bluez Bluetooth stack.
 #
 # The Bluetooth connection sometimes drops, but gatttool fails to 
 # exit. There is currently no timeout options for gatttool. To overcome
 # this limitation the process is killed and restarted if no BT events
-# arrive in 10s. To achieve this timeout a pipe must be used as bash
+# arrive in 10 seconds. To achieve this timeout a pipe must be used as bash
 # 'read' timeout does not work when reading from [??]
 #
 # http://stackoverflow.com/questions/4874993/bash-script-with-non-blocking-read/4875924#4875924
 #
 
-# TODO: del FIFO on exit
-# TODO: kill all subprocesses
-
-# Bluetooth address of Heart Rate monitor
+# Bluetooth address of Heart Rate monitor. Use "hcitool lescan" to discover.
 BTADDR=D0:39:72:D3:11:50
 
-# Retart gatttool if no messages received in this number of seconds
+# Restart gatttool if no messages received in this number of seconds
 TIMEOUT=10
 
 # File descriptor used for pipe
@@ -38,31 +37,28 @@ exec 7<>${FIFO}
 # http://stackoverflow.com/questions/360201/kill-background-process-when-shell-script-exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-
+# Loop forever
 while true ; do 
 
 	echo "Staring gatttool"
 	
 	# Send gatttool into file handle 7
-	gatttool -b $BTADDR --char-write-req -a  0x13 -n 0100 --listen >&$FD &
-	#ping -i 10 8.8.8.8 >&$FD &
+	gatttool -b $BTADDR --char-write-req -a  0x13 -n 0100 --listen >&7  &
 	gatttool_pid=$!
 
-	while read -t $TIMEOUT -u $FD line ; do
-		read_exit_code=$?
+	while read -t $TIMEOUT -u 7 line ; do
+		#read_exit_code=$?
   	 	a=( $line )
 
-		#echo "exit_code=$read_exit_code"
-
 		# Extract col index 6 and convert from hex to dec
-		hr=$(( 16#${a[6]} ))	
-		echo "`date +%s` $hr $read_exit_code"
-		#echo "`date +%s` $line"
+		hr=$(( 16#${a[6]} ))
+
+		# Output <unix-time> <heart-rate>	
+		echo "`date +%s` $hr"
 	done
 
 	#kill -- -$BASHPID
-	echo "killing $gatttool_pid"
+	echo "killing gatttool on process $gatttool_pid"
 	kill $gatttool_pid	
 done
-
 
